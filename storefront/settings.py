47eeb853c27864a5db3ceab6e3dfa821fd70f2e4
Emily Santos <eyssantos@gmail.com>
2023-01-25 14:56:43 +0000

    create store and tags apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +50,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'storefront.urls'
+
 
 TEMPLATES = [
     {
